444ee9fecce217e0e82aa419773b4eadd163aa38
Andrew-157 <subotinandrey5@gmail.com>
2025-10-02 19:13:17 +0200

    moved letters to header

---
+++ skill_issuer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include "skill_issuer.h"
+
+int main() {
+    printf("%s\n", S);
+    printf("%s\n", K);
+    printf("%s\n", I);
+    printf("%s\n", L);
+    printf("%s\n", L);
+    printf("\n\n");
+    printf("%s\n", I);
+    printf("%s\n", S);
+    printf("%s\n", S);
+    printf("%s\n", U);
+    printf("%s", E);
+}
